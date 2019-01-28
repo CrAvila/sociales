98c430f0b8c0fbf2d1ba28d1e7284c7fe555889b
CR_Avila <carlosrobertoavilah@gmail.com>
2019-01-27 20:34:15 -0600

    Upload sociales files

---
+++ Sociales/main.js
@@ -0,0 +1,18 @@
+// function name() {
+    
+    //code to be executed
+  
+  //}
+
+  juegos 3ds
+
+  https://mega.nz/#!0cNXBTSL!PjDmIBaw9UcVujaxoPtUAso0h0tV-sRPKthhwRgWwqQ
+  https://mega.nz/#!EI1CiJ6S!ad1g-GAx75AMbwuLeLHTGAr9h8X8wSLlml9gd8_4aaI
+
+  https://mega.nz/#!M5k1UThD!RwCTHbKZMeBlNpH_0_5hLcPYIWegtm2081BpiB88sT0
+  https://mega.nz/#!1JdQjIZB!6bOdoiHyGVX0Na1bpQ6hmlRN5a0URk9KzEJYF3ztlKs
+  https://mega.nz/#!cZd3xbwB!8nBkOmSzhRPYvUKgWO2apqdj7jgwD5_dD9zNpMPmSbM
+  
+  https://mega.nz/#!B8FVzJYD!v6u2yFt8jBK_c7vbZvC1Gv_3HC9hhJkBhTYJR8e13Uc
+
+  https://mega.nz/#F!eQQn2D5A!gOMZWpcRJ7aoPcbq53KiDQ
